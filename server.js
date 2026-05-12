a5f6520e1c9a413e88c406b5c4b54e7e48c71eff
Abdelrahman Mohamed <78263461+Soooonic@users.noreply.github.com>
2026-05-12 14:39:59 +0300

    add new endpoint

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import express from 'express';
 const app=express();
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://11410120201112_db_user:5vjmmFOFTJUP1pNV@cluster0.jqhtx7h.mongodb.net/?appName=Cluster0';
 
 mongoose.connect(mongoURI)
   .then(() => console.log('Connected to MongoDB Replica Set'))
@@ -13,7 +13,7 @@ mongoose.connect(mongoURI)
 const Post = mongoose.model('Post', { title: String, content: String });
 
 app.get('/', (req, res) => res.send('Welcome to my distributed backend system'));
-app.get('/health', (req, res) => res.send('Healthy'));
+app.get('/health', (req, res) => res.status(200).send('Healthy'));
 
 app.get('/posts', async (req, res) => {
     const posts = await Post.find();
@@ -36,6 +36,11 @@ app.delete('/posts/:id', async (req, res) => {
     const post = await Post.findByIdAndDelete(req.params.id);
     if (!post) return res.status(404).send('Post not found');
     res.send({ message: 'Post deleted successfully' });
+});
+app.get('/posts/:id', async (req, res) => {
+    const post=await Post.findById(req.params.id);
+    if (!post) return res.status(404).send('Post not found');
+    res.send(post);
 });
 
 const PORT = 3000;
